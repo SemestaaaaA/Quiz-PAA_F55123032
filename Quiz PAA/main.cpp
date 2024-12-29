1c8b2042f67f24ef3f2a615a77eb82320cb5a2ae
Nakita Semestaaa <mrsaladasss@gmail.com>
2024-12-29 19:53:12 +0800

    first commit

---
+++ Quiz PAA/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // ini buat Deklarasi variabel
+    int i = 0;
+    int m;
+
+    // buatin untuk perulangan while
+    while (i < 9) {
+        m = i * i;
+        cout << m << endl;
+        i = i + 1;
+    }
+
+    return 0;
+}
+
